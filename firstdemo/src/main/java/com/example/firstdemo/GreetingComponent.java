6e2ac4e92c5a33e12ec3c2fdda3aa15cfa97c91e
asmalizaa <asmalizaa@gmail.com>
2024-09-03 14:37:51 +0800

    First commit

---
+++ firstdemo/src/main/java/com/example/firstdemo/GreetingComponent.java
@@ -0,0 +1,24 @@
+package com.example.firstdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingComponent {
+
+	public String getMessage() {
+		return "Hello from GreetingComponent";
+	}
+	
+	public String postMessage() {
+		return "Post message to GreetingComponent";
+	}
+	
+	public String putMessage() {
+		return "Put message to GreetingComponent";
+	}
+	
+	public String deleteMessage() {
+		return "Delete message to GreetingComponent";
+	}
+
+}
